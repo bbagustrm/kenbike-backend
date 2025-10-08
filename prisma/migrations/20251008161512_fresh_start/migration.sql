1691568fe7a0e4ee66791176f2af7bb98e729ad6
Bagus Tri Atmojo <100054889+bbagustrm@users.noreply.github.com>
2025-10-08 23:27:48 +0700

    fix(prisma): error seed vps

--- prisma/migrations/20251008161512_fresh_start/migration.sql
+++ prisma/migrations/20251008161512_fresh_start/migration.sql
@@ -1,42 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `BlacklistedToken` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `PasswordReset` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `RefreshToken` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "public"."BlacklistedToken" DROP CONSTRAINT "BlacklistedToken_user_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "public"."PasswordReset" DROP CONSTRAINT "PasswordReset_user_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "public"."RefreshToken" DROP CONSTRAINT "RefreshToken_user_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "public"."VerificationToken" DROP CONSTRAINT "VerificationToken_user_id_fkey";
-
--- DropTable
-DROP TABLE "public"."BlacklistedToken";
-
--- DropTable
-DROP TABLE "public"."PasswordReset";
-
--- DropTable
-DROP TABLE "public"."RefreshToken";
-
--- DropTable
-DROP TABLE "public"."User";
-
--- DropTable
-DROP TABLE "public"."VerificationToken";
-
--- DropEnum
-DROP TYPE "public"."AuthProvider";
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN', 'OWNER');
 
 -- CreateTable
 CREATE TABLE "public"."users" (
