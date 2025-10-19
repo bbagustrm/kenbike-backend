70b2af7865f90f5c10a545d0ebbb3f6156861bb2
Bagus Tri Atmojo <100054889+bbagustrm@users.noreply.github.com>
2025-10-19 23:50:27 +0700

    fix: storage CDN

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import helmet from 'helmet';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
@@ -19,6 +21,10 @@ async function bootstrap() {
 
   // Set global prefix untuk semua routes
   app.setGlobalPrefix('api/v1');
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   await app.listen(3000);
   logger.log('info', '🚀 Server running on http://localhost:3000/api/v1');
