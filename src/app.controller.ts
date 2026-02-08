ff37ad44b72cc1496e5ea538b7ed81be1575ec38
Bagus Tri Atmojo <100054889+bbagustrm@users.noreply.github.com>
2026-02-08 20:26:20 +0700

    test: deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import {Public} from "./common/decorators/public.decorator";
 
 @Controller()
 export class AppController {
+
     @Get()
-    getHello() {
-        return { message: 'Hello World!' };
+    root() {
+        return {
+            message: 'KenBike API is running',
+        };
+    }
+
+    @Public()
+    @Get('health')
+    health() {
+        return {
+            status: 'ok',
+            service: 'kenbike-backend',
+            timestamp: new Date().toISOString(),
+        };
     }
 }
